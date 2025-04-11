38b90af62f1e589cd0d57b852e04d51171dd8ec2
Imggaggu <ggaggu@snu.ac.kr>
2025-04-11 12:36:11 +0900

    [Fix] Add overflow error

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,14 @@
 #include "calculator.h"
 #include <stdexcept>
+#include <limits>
 
 int Calculator::add(int a, int b) {
     // TODO
-    return a + b;
+    int max = std::numeric_limits<int>::max();
+    if ((b > 0) && (a > max - b)){
+        throw std::overflow_error ("input Overflow");
+    } 
+    return a+b;
 }
 
 int Calculator::sub(int a, int b) {
