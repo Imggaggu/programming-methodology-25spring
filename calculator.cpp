4943992469d77c8a0d229c9d757d0bc7afb31985
Imggaggu <ggaggu@snu.ac.kr>
2025-04-11 12:45:24 +0900

    [Fix] multiply Overflow

--- calculator.cpp
+++ calculator.cpp
@@ -31,6 +31,12 @@ int Calculator::sub(int a, int b) {
 
 int Calculator::mul(int a, int b) {
     // TODO
+    int max = std::numeric_limits<int>::max();
+
+    if ((b > 0) && (a > max / b)){
+        throw std::overflow_error ("multiply Overflow");
+    } 
+
     return a * b ;
 }
 
